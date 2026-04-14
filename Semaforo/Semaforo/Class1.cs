ca2e9c1980b571d4929e2f8c5cf36e56221070ac
julietasegovia <julietavsegovia@gmail.com>
2026-04-14 16:10:29 -0300

    Add files via upload

---
+++ Semaforo/Semaforo/Class1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Semaforo
+{
+    public class Semaforo
+    {
+        public string color;
+        public int segundos = 0;
+
+        public string mostrarColor()
+        {
+            return color;
+        }
+
+        public void ponerEnIntermitente() 
+        {
+            if (segundos % 2 == 0)
+                color = "Amarillo";
+            else
+                color = "Apagado";
+
+            segundos++;
+        }
+
+        public void sacarDeIntermitente()
+        {
+            pasoDelTiempo(segundos);
+        }
+
+        public void pasoDelTiempo(int mas_segundos)
+        {
+            segundos += mas_segundos;
+
+            bool cambio = true;
+
+            while (cambio) {
+
+                cambio = false;
+
+                if (color == "Rojo" && segundos >= 30)
+                {
+                    color = "Rojo + Amarillo";
+                    segundos -= 30;
+                    cambio = true;
+                }
+
+                 else if (color == "Rojo + Amarillo" && segundos >= 2)
+                {
+                    color = "Verde";
+                    segundos -= 2;
+                    cambio = true;
+                }
+
+                else if (color == "Verde" && segundos >= 20)
+                {
+                    color = "Amarillo";
+                    segundos -= 20;
+                    cambio = true;
+                }
+
+                else if (color == "Amarillo" && segundos >= 2)
+                {
+                    color = "Rojo";
+                    segundos -= 2;
+                    cambio = true;
+                }
+            }
+        }
+    }
+
+}
---
+++ Semaforo/Semaforo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Semaforo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
